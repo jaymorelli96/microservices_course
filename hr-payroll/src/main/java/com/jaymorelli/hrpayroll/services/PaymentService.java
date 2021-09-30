7ee19b6f6ade07108b3c1c2d236737ce849bfe1b
jaymorelli96 <jcrmorelli@gmail.com>
2021-09-30 11:44:46 +0100

    Ribbon load balacing

--- hr-payroll/src/main/java/com/jaymorelli/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/jaymorelli/hrpayroll/services/PaymentService.java
@@ -1,16 +1,11 @@
 package com.jaymorelli.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.jaymorelli.hrpayroll.entities.Payment;
 import com.jaymorelli.hrpayroll.entities.Worker;
 import com.jaymorelli.hrpayroll.feignclients.WorkerFeignClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
