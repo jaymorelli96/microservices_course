3a00c68f75ced54d865d701e28576f4781eb5c88
jaymorelli96 <jcrmorelli@gmail.com>
2021-10-04 10:08:07 +0100

    hr-user created

---
+++ hr-user/src/test/java/com/jaymorelli/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaymorelli.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
